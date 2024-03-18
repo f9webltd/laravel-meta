5fc3911ff609e1fb193c1011237939c71c90af5b
Robert Allport <rallport@rallport.com>
2024-03-18 09:49:17 +0000

    Update meta helper
    Previously the `meta` helper method could return a specific meta tag i.e. `meta('my-tag')`. This is a shortcut to writing `meta()->get('my-tag')`.
    
    The same function could also return an instance of the main `Meta` class.
    
    This caused autocompletion issues. The helper function now returns an instance of the meta class.

--- src/helpers.php
+++ src/helpers.php
@@ -3,18 +3,8 @@
 use F9Web\Meta\Meta;
 
 if (!function_exists('meta')) {
-    /**
-     * @param  string|null  $key
-     * @return array|\F9Web\Meta\Meta|string|null
-     */
-    function meta(?string $key = null)
+    function meta(?string $key = null): Meta
     {
-        $instance = resolve(Meta::class);
-
-        if ($key === null) {
-            return $instance;
-        }
-
-        return $instance->get($key);
+        return resolve(Meta::class);
     }
 }
